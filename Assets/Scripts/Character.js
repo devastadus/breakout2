3b8012ae4e835d8a5d7ec24c83d71b79585fce22
devastadus <scottprice7@gmail.com>
2014-11-02 23:02:19 -0800

    update

--- Assets/Scripts/Character.js
+++ Assets/Scripts/Character.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 var lastX:float;
 var isMoving:boolean = false;
+var catchAudio:AudioClip;
 
 
 function Start () {
@@ -15,17 +16,37 @@ function Update () {
 		//if(!isMoving){
 		//Debug.Log('moving');
 			isMoving = true;
-			animation.CrossFade("step");				
+			if(!animation.IsPlaying("catch")){			
+				 animation.CrossFade("step");	
+				Debug.Log("step");						
+			}						
 		//}
 	}
 	else{
-	//	if(isMoving){
+		//if(isMoving){
 		//Debug.Log('stop');
 			isMoving = false;
-			animation.CrossFade("idle");				
-	//	}		
+			if(!animation.IsPlaying("catch")){			
+				Debug.Log("idle");
+				animation.CrossFade("idle");
+			}					
+				
+		//}		
 	}		
-	lastX = transform.position.x;	
+	lastX = transform.position.x;			
+}
+
+function OnCollisionEnter(col : Collision) {
+	if(col.gameObject.CompareTag("bomb")){		
+		
+	} else if(col.gameObject.CompareTag("stein")){		
+		audio.PlayOneShot(catchAudio);
+		Debug.Log('catch');
+		animation.Play("catch");
+	}
 	
+	col.gameObject.transform.position.y = 50;
+	col.gameObject.transform.position.z = -16;
+	col.gameObject.transform.position.x = Random.Range(0,60);
 	
 }
--- Assets/Scripts/Falling Object.js
+++ Assets/Scripts/Falling Object.js
@@ -2,15 +2,16 @@
 
 var prefab:GameObject;
 var speed:int;
+var audioClips:AudioClip[];
 
 function Start () {
 
 }
 
 function Update () {
-	transform.position.y -= speed * Time.deltaTime;
-	Debug.Log(Time.deltaTime);
+	transform.position.y -= speed * Time.deltaTime;	
 	if(transform.position.y < 0){
+		audio.PlayOneShot(audioClips[Random.Range(0,audioClips.length)]);
 		Instantiate(prefab, transform.position,Quaternion.identity);
 		transform.position.y = 50;
 		transform.position.x = Random.Range(0,60);
